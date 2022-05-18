e64bee9211390fff5891b7be31b17961f4cf3cb0
aryapangestu <aryapangestu02@gmail.com>
2022-05-18 09:50:55 +0700

    clean code

--- perjalanan-app/app/Http/Controllers/RegisterController.php
+++ perjalanan-app/app/Http/Controllers/RegisterController.php
@@ -40,49 +40,4 @@ class RegisterController extends Controller
         User::create($validated);
         return redirect('/login');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
