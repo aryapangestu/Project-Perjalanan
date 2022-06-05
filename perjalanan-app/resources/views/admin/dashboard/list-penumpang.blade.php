37b973a5048e513d0ab4c9306f274bc72593a8cc
SyafiqMA <syafiqlira21@gmail.com>
2022-06-05 23:02:37 +0700

    View Modal Admin

--- perjalanan-app/resources/views/admin/dashboard/list-penumpang.blade.php
+++ perjalanan-app/resources/views/admin/dashboard/list-penumpang.blade.php
@@ -46,9 +46,31 @@
                                                 </div>
                                             </td>
                                             <td>
-                                                <div class="d-flex">
+                                                {{-- <div class="d-flex">
                                                     <a href="/list-penumpang/{{ $user->id }}/view"
                                                         class="btn btn-info" style="margin-right:2px">View</a>
+                                                </div> --}}
+                                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalview{{ $user->id }}">
+                                                    View
+                                                </button>
+
+                                                <!-- Modal -->
+                                                <div class="modal fade" id="modalview{{ $user->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                        Info
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                                        </div>
+                                                    </div>
+                                                    </div>
                                                 </div>
                                             </td>
                                         </tr>
